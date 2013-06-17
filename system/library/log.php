bc8e2b5c28669bf24c6218e57d92496d9d6ee909
CZechBoY <czechboy@jabko.net>
2013-06-18 00:26:32 +0200

    changed open log file to write only (append)

--- system/library/log.php
+++ system/library/log.php
@@ -3,17 +3,15 @@ class Log {
 	private $filename;
 	
 	public function __construct($filename) {
-		$this->filename = $filename;
+            $this->filename = $filename;
 	}
 	
 	public function write($message) {
-		$file = DIR_LOGS . $this->filename;
-		
-		$handle = fopen($file, 'a+'); 
-		
-		fwrite($handle, date('Y-m-d G:i:s') . ' - ' . $message . "\n");
-			
-		fclose($handle); 
+            $file = DIR_LOGS . $this->filename;
+
+            $handle = fopen($file, 'a'); 
+            fwrite($handle, date('Y-m-d G:i:s') . ' - ' . $message . "\n");
+            fclose($handle); 
 	}
 }
 ?>
