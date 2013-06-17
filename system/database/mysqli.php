12582688ea25e04c9decd0e2fb6c955cdba89f93
CZechBoY <czechboy@jabko.net>
2013-06-18 00:13:07 +0200

    changed mysqli_query("SET NAMES..") to mysqli->set_charset

--- system/database/mysqli.php
+++ system/database/mysqli.php
@@ -3,71 +3,59 @@ final class MySQLi {
 	private $mysqli;
 	
 	public function __construct($hostname, $username, $password, $database) {
-		$this->mysqli = new mysqli($hostname, $username, $password, $database);
-		
-		if ($this->mysqli->connect_error) {
-      		trigger_error('Error: Could not make a database link (' . $this->mysqli->connect_errno . ') ' . $this->mysqli->connect_error);
-		}
-		
-		$this->mysqli->query("SET NAMES 'utf8'");
-		$this->mysqli->query("SET CHARACTER SET utf8");
-		$this->mysqli->query("SET CHARACTER_SET_CONNECTION=utf8");
-		$this->mysqli->query("SET SQL_MODE = ''");
+            $this->mysqli = new mysqli($hostname, $username, $password, $database);
+
+            if ($this->mysqli->connect_error) {
+                trigger_error('Error: Could not make a database link (' . $this->mysqli->connect_errno . ') ' . $this->mysqli->connect_error);
+            }
+
+            $this->mysqli->set_charset("utf8");
   	}
 		
   	public function query($sql) {
-		$result = $this->mysqli->query($sql);
+            $result = $this->mysqli->query($sql);
+            
+            if ($this->mysqli->errno) {
+                if (is_resource($result)) {
+                    $data = array();
 
-		
+                    while($row = $result->fetch_object()) {
+                            $data[] = $row;
+                    }
 
-		if ($this->mysqli->errno) {
-			if (is_resource($resource)) {
-				$i = 0;
-    	
-				$data = array();
-		
-				while ($row = $result->fetch_object()) {
-					$data[$i] = $row;
-    	
-					$i++;
-				}
+                    $result->close();
 
-				$result->close();
-				
-				$query = new stdClass();
-				$query->row = isset($data[0]) ? $data[0] : array();
-				$query->rows = $data;
-				$query->num_rows = $result->num_rows;
-				
-				unset($data);
-				
-				
-				
-				
-				return $query;	
-    		} else {
-				return true;
-			}
-		} else {
-			trigger_error('Error: ' . mysql_error($this->link) . '<br />Error No: ' . mysql_errno($this->link) . '<br />' . $sql);
-			exit();
-    	}
+                    $query = new stdClass();
+                    $query->row = isset($data[0]) ? $data[0] : array();
+                    $query->rows = $data;
+                    $query->num_rows = $result->num_rows;
+
+                    unset($data);
+                    
+                    return $query;	
+                } else {
+                        return true;
+                }
+            } else {
+                trigger_error('Error: ' . mysql_error($this->link) . '<br />Error No: ' . mysql_errno($this->link) . '<br />' . $sql);
+                exit();
+            }
   	}
 	
 	public function escape($value) {
-		return $this->mysqli->real_escape_string($value);
+            return $this->mysqli->real_escape_string($value);
 	}
 	
   	public function countAffected() {
-    	return $this->mysqli->affected_rows;
+            return $this->mysqli->affected_rows;
   	}
 
   	public function getLastId() {
-    	return $this->mysqli->insert_id;
+            return $this->mysqli->insert_id;
   	}	
 	
 	public function __destruct() {
-		$this->mysqli->close();
+            $this->mysqli->close();
 	}
 }
 ?>
